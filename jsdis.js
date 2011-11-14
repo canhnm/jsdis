c52383ae7e045bcc1146808c9f02c48f71fe2ecd
canhnm <canhnm@opensource.com.vn>
2011-11-14 11:19:52 +0700

    update

--- jsdis.js
+++ jsdis.js
@@ -1,144 +1,134 @@
 var jsdis = {
-		config: function(params){
-			//assign default driver
-			if( params.driver == '' || (params.driver!='localStorage' && params.driver!='sessionStorage') ){
-				params.driver = 'localStorage';
+	//check if localStorage is supported in the browser
+	isSupported: function(){
+		return ('localStorage' in window) && window.localStorage !== null;
+	},
+	//check if JSON is supported in the browser
+	isJSON: function(){
+		return window.JSON != null;
+	},
+	driver: function(){
+		
+	},
+	exists: function(key){
+		return this.storage.getItem( key ) != null;
+	},
+	set: function(key, value) {
+		if (!this.isSupported) return;
+		
+		try {
+			if (typeof value == "object") {
+				//we use JSON.stringify because localStored only stores data in string
+				value = JSON.stringify(value);
 			}
-			this.config = {
-				'driver': params.driver
-			};
-		},
-		//check if localStorage is supported in the browser
-		isSupported: function(){
-		    return ('localStorage' in window) && window.localStorage !== null;
-		},
-	  	//check if JSON is supported in the browser
-	  	isJSON: function(){
-			return window.JSON != null;
-		},
-		driver: function(){
-			
-		},
-		exists: function(key){
-			return this.storage.getItem( key ) != null;
-		},
-		set: function(key, value) {
-			if (!this.isSupported) return;
-			
-			try {
-		        if (typeof value == "object") {
-		        	//we use JSON.stringify because localStored only stores data in string
-		            value = JSON.stringify(value);
-		        }
-	        	localStorage.setItem(key, value);
-	      	} catch (e) {
-	      		return e;
-	      	}
-	      	return true;
-	    },
-	    get: function(key) {
-			if (!this.isSupported) return;
+			localStorage.setItem(key, value);
+		} catch (e) {
+			return e;
+		}
+		return true;
+	},
+	get: function(key) {
+		if (!this.isSupported) return;
 
-	        var value = localStorage.getItem(key),
-	        	valLengh = value.length;
-        	//we use JSON.parse to return value that stringified
-	        if (value[0] == "{" && value[valLength] == "}") {
-	            value = JSON.parse(value);
-	        }
-	        return value;
-	    },
-	    //Set the string value of a key and return its old value 
-	    getset: function(key, value){
-	    	var oldVal = this.get(key);
-	    	this.set(key,value);
-	    	return oldVal;
-	    },
-	    //delete by key given
-	    del: function(key) {
-			if (!this.isSupported) return;
+		var value = localStorage.getItem(key),
+			valLengh = value.length;
+		//we use JSON.parse to return value that stringified
+		if (value[0] == "{" && value[valLength] == "}") {
+			value = JSON.parse(value);
+		}
+		return value;
+	},
+	//Set the string value of a key and return its old value 
+	getset: function(key, value){
+		var oldVal = this.get(key);
+		this.set(key,value);
+		return oldVal;
+	},
+	//delete by key given
+	del: function(key) {
+		if (!this.isSupported) return;
 
-	    	return localStorage.removeItem("name");
-	    },
-	    // Increment the integer value of a key by one 
-	    incr: function(key){
-    		var curVal = this.get(key);
-    		if(curVal){
-    			//check if value of this key is integer
-		    	if(isNaN(parseInt(curVal,10))===false){
-		    		var newVal = parseInt(curVal)+1;
-		    		return this.set(key, newVal);
-		    	}else{
-		    		return 'Value of key is not integer!';
-		    	}	
-    		}else{
-    			return 'No value found!';
-    		}
-	    },
-	    // Increment the integer value of a key by given number value 
-	    incrby: function(key, increment){
-    		var curVal = this.get(key);
-    		if(curVal){
-    			//check if value of this key is integer
-		    	if(isNaN(parseInt(curVal,10))===false){
-		    		var newVal = parseInt(curVal)+increment;
-		    		return this.set(key, newVal);
-		    	}else{
-		    		return 'Value of key is not integer!';
-		    	}
-    		}else{
-    			return 'No value found!';
-    		}
-	    },
-	    //decrement the integer value of key by one
-	    decr: function(key){
-    		var curVal = this.get(key);
-    		if(curVal){
-    			//check if value of this key is integer
-		    	if(isNaN(parseInt(val,10))===false){
-		    		var newVal = curVal-1;
-		    		this.set(key, newVal);
-		    	}else{
-	    			return 'Value of key is not integer!';
-		    	}	
-    		}else{
-    			return 'No value found!';
-    		}
-	    },
-	    //decrement the integer value of this key by the given number value
-	    decrby: function(key, decrement){
-    		var curVal = this.get(key);
-    		if(curVal){
-    			//check if value of this key is integer
-		    	if(isNaN(parseInt(val,10))===false){
-		    		var newVal = curVal-decrement;
-		    		this.set(key, newVal);
-		    	}
-    		}
-	    	return 'Value of key is not integer!';
-	    },
-	    //Rename a key
-	    rename: function(key, newkey) {
-	    	var curVal = this.get(key);
-	    	this.del(key);
-	    	this.set(newkey, curVal);
-	      	return true;
-	    },
-	    //Get the length of the value stored in a key
-	    strlen: function(key){
-	    	var curVal = this.get(key);
-	    	return strlen(curVal);
-	    },
-	    //clear current stored value
-	    flush: function(){
-			if (!this.isSupported) return;
+		return localStorage.removeItem("name");
+	},
+	// Increment the integer value of a key by one 
+	incr: function(key){
+		var curVal = this.get(key);
+		if(curVal){
+			//check if value of this key is integer
+			if(isNaN(parseInt(curVal,10))===false){
+				var newVal = parseInt(curVal)+1;
+				return this.set(key, newVal);
+			}else{
+				return 'Value of key is not integer!';
+			}	
+		}else{
+			return 'No value found!';
+		}
+	},
+	// Increment the integer value of a key by given number value 
+	incrby: function(key, increment){
+		var curVal = this.get(key);
+		if(curVal){
+			//check if value of this key is integer
+			if(isNaN(parseInt(curVal,10))===false){
+				var newVal = parseInt(curVal)+increment;
+				return this.set(key, newVal);
+			}else{
+				return 'Value of key is not integer!';
+			}
+		}else{
+			return 'No value found!';
+		}
+	},
+	//decrement the integer value of key by one
+	decr: function(key){
+		var curVal = this.get(key);
+		if(curVal){
+			//check if value of this key is integer
+			if(isNaN(parseInt(val,10))===false){
+				var newVal = curVal-1;
+				this.set(key, newVal);
+			}else{
+				return 'Value of key is not integer!';
+			}	
+		}else{
+			return 'No value found!';
+		}
+	},
+	//decrement the integer value of this key by the given number value
+	decrby: function(key, decrement){
+		var curVal = this.get(key);
+		if(curVal){
+			//check if value of this key is integer
+			if(isNaN(parseInt(val,10))===false){
+				var newVal = curVal-decrement;
+				this.set(key, newVal);
+			}
+		}
+		return 'Value of key is not integer!';
+	},
+	//Rename a key
+	rename: function(key, newkey) {
+		var curVal = this.get(key);
+		this.del(key);
+		this.set(newkey, curVal);
+		return true;
+	},
+	//Get the length of the value stored in a key
+	strlen: function(key){
+		var curVal = this.get(key);
+		return strlen(curVal);
+	},
+	//clear current stored value
+	flush: function(){
+		if (!this.isSupported) return;
 
-	    	if(typeof(localStorage) != 'undefined'){ 
-				return localStorage.clear() ;
-			} 
-			else{ 
-				return "window.localStorage is not defined"; 
-			}    
-	      	return true;	
-	    }
-	
+		if(typeof(localStorage) != 'undefined'){ 
+			return localStorage.clear() ;
+		} 
+		else{ 
+			return "window.localStorage is not defined"; 
+		}    
+		return true;	
+	}
 }
--- readme.txt
+++ readme.txt
@@ -6,3 +6,15 @@ Contact : canhnm@opensource.com.vn
 Store value in localStorage of browser in the way like redis.
 See index.html for demo code!
 Have fun!!!
+
+--=========================================================================--
+The reason to use localStorage :
++ sessionStorage sets fields on the window. When the window is closed, 
+	the session fields are lost, even if the site remains open in another window.
++ localStorage sets fields on the domain. Even when you close the browser, 
+	reopen it, and go back to the site, it remembers all fields in localStorage.
+
+So, I need a persistence data storage.
+
+The size of localStorage is limited by each browser, if you want to control your own space let see Manifest!
+--=========================================================================--
